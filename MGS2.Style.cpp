354c0fb672aaad5639a4350b6c0c29b5b1aeb602
Ian Bennett <webdev@ianbennett.net>
2024-06-21 22:14:00 +0100

    [Style] Support multiple values for CodenameSecret with random selection

--- MGS2.Style.cpp
+++ MGS2.Style.cpp
@@ -5,10 +5,11 @@ namespace MGS2::Style {
 
 	namespace CodenameSecret {
 		const char* Category = Style::Category;
-		const size_t START_ID = 0;
+		const size_t START_ID = 1;
 		const size_t END_ID = 3531;
-
-		size_t VoxTrackId = 0x547; // default "What do you think you're doing!"
+		const size_t DEFAULT_ID = 0x547; // "What do you think you're doing!"
+		
+		std::vector<size_t> VoxTrackIds;
 
 		tFUN_Void_Int oFUN_00751700;
 		void __cdecl hkFUN_00751700(int param_1) {
@@ -17,19 +18,32 @@ namespace MGS2::Style {
 			if (*(int*)(param_1 + 0x130) != 0) return;
 			if (*(int*)(param_1 + 300) != 10) return;
 
-			Vox::Play(VoxTrackId);
+			size_t count = VoxTrackIds.size();
+			if (!count) return;
+
+			size_t selectedIndex = MGS2::RNG(count);
+			size_t selectedId = VoxTrackIds[selectedIndex];
+			Vox::Play(selectedId);
 		}
 
 		void Run(CSimpleIniA& ini) {
 			const char* subOption = "CodenameSecret";
-			int codenameSecret = ConfigParser::ParseInteger(ini, Category, subOption, -1, (int)START_ID, (int)END_ID, true);
-			if (codenameSecret == -1) {
-				codenameSecret = ini.GetBoolValue(Category, subOption, true) ? VoxTrackId : -1;
+
+			CSimpleIniA::TNamesDepend entries;
+			ini.GetAllValues(Category, subOption, entries);
+			for (const CSimpleIniA::Entry& entry : entries) {
+				int input = atoi(entry.pItem);
+				int voxId = ConfigParser::ParseInteger(input, 0, (int)START_ID, (int)END_ID, true);
+				if (voxId) {
+					VoxTrackIds.push_back(voxId);
+				}
 			}
-			else {
-				VoxTrackId = codenameSecret;
+
+			if ((VoxTrackIds.empty()) && (ini.GetBoolValue(Category, subOption, true))) {
+				VoxTrackIds.push_back(DEFAULT_ID);
 			}
-			if (codenameSecret != -1) {
+
+			if (!VoxTrackIds.empty()) {
 				oFUN_00751700 = (tFUN_Void_Int)mem::TrampHook32((BYTE*)0x751700, (BYTE*)hkFUN_00751700, 5);
 			}
 		}
