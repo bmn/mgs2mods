0472fc51e9449cb9640e09952f160444b3c262dd
Ian Bennett <webdev@ianbennett.net>
2024-06-21 21:15:11 +0100

    Release b3p version change and docs

--- ASI.cpp
+++ ASI.cpp
@@ -10,7 +10,7 @@ namespace ASI {
 		return GetRawVersion(major, charMinor, version, charBuild);
 	}
 
-	char Version[] = "b3o";
-	unsigned int RawVersion = GetRawVersionFromChars(0, 'b', 3, 'o');
+	char Version[] = "b3p";
+	unsigned int RawVersion = GetRawVersionFromChars(0, 'b', 3, 'p');
 
 }
